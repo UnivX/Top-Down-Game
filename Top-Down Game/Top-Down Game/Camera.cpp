045e04467422c126a9fefbc24c059e5e50d85c19
SIMONE MARELLA <simonemarella@hotmail.it>
2020-05-28 15:03:10 +0200

    Camera Class Finished
    finita la base della classe Camera

--- Top-Down Game/Top-Down Game/Camera.cpp
+++ Top-Down Game/Top-Down Game/Camera.cpp
@@ -4,6 +4,14 @@
 
 Camera::Camera()
 {
+	this->followed_entity;
+	this->m_trauma = 0;
+	this->trauma_reducing = 0.1;
+	this->m_camera_smoothness = 2;
+	this->maxAngleShaking = 2;
+	this->maxOffSetShaking = 2;
+	this->m_angle = 0;
+	this->m_pos = sf::Vector2f(0, 0);
 }
 
 
@@ -14,11 +22,112 @@ Camera::~Camera()
 void Camera::Update(float dt)
 {
 	//computing camera shake
+	ShakingData shake = this->ComputeCameraShake(dt);
+
+	//compute new camera position in base of traked entity
+	if (this->m_camera_smoothness == 0)
+		this->m_pos = this->followed_entity.lock()->GetPosition();
+
+	if (!this->followed_entity.expired()) {
+		std::shared_ptr<Entity> entity = this->followed_entity.lock();
+
+		sf::Vector2f distance = entity->GetPosition() - m_pos;
+		sf::Vector2f smoothDistance = distance * (1.f / this->m_camera_smoothness);
+
+		this->m_pos += smoothDistance * dt;
+	}
+
+	this->view.setCenter(this->m_pos + this->m_camera_position_offset + shake.transitional);
+	this->view.setRotation(this->m_angle + shake.rotational);
+
+	//get direction be
+
+}
+
+void Camera::SetViewToTarget(sf::RenderTarget& target)
+{
+	this->view.setViewport(target.getDefaultView().getViewport());
+	target.setView(this->view);
 }
 
 void Camera::AddCameraShake(float force)
 {
 	this->m_trauma += force;
+}
+
+void Camera::SetCameraShake(float force)
+{
+	this->m_trauma = force;
+}
+
+void Camera::SetEntityToFollow(std::weak_ptr<Entity> entity)
+{
+	this->followed_entity = entity;
+}
+
+void Camera::SetTraumaReducing(float tr)
+{
+	this->trauma_reducing = tr;
+}
+
+float Camera::GetTraumaReducing()
+{
+	return this->trauma_reducing;
+}
+
+float Camera::GetMaxAngleShaking()
+{
+	return maxAngleShaking;
+}
+
+void Camera::SetMaxAngleShaking(float maxAngle)
+{
+	this->maxAngleShaking = maxAngle;
+}
+
+float Camera::GetMaxOffSetShaking()
+{
+	return this->maxOffSetShaking;
+}
+
+void Camera::SetMaxOffSetShaking(float offset)
+{
+	this->maxOffSetShaking = offset;
+}
+
+float Camera::GetCameraSmoothness()
+{
+	return this->m_camera_smoothness;
+}
+
+void Camera::SetCameraSmoothness(float smoothness)
+{
+	this->m_camera_smoothness = smoothness;
+}
+
+float Camera::GetCameraAngle()
+{
+	return this->m_angle;
+}
+
+void Camera::SetCameraAngle(float angle)
+{
+	this->m_angle = angle;
+}
+
+sf::Vector2f Camera::GetCameraPositionOffSet()
+{
+	return this->m_camera_position_offset;
+}
+
+void Camera::SetCameraPositionOffSet(sf::Vector2f offset)
+{
+	this->m_camera_position_offset = offset;
+}
+
+float Camera::GetCameraTrauma()
+{
+	return this->m_trauma;
 }
 
 ShakingData Camera::ComputeCameraShake(float dt)
@@ -27,7 +136,21 @@ ShakingData Camera::ComputeCameraShake(float dt)
 	sf::Vector2f finalVector(0, 0);
 	float finalRotation = 0;
 
-	
+	float shake = pow(shake, 2);//get shake value, is pow of 2 for more nice effect, it can be also of 3
+
+	//formula to get shaking effect = MaxOffset * dhake * random_number
+
+	//generate random number from -1 to 1
+	float randomNumber = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / 2)) -1;
+	finalRotation = this->maxAngleShaking * shake * randomNumber;
+
+	//generate another random number
+	randomNumber = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / 2)) - 1;
+	finalVector.x = this->maxOffSetShaking * shake * randomNumber;
+
+	//generate another random number
+	randomNumber = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / 2)) - 1;
+	finalVector.y = this->maxOffSetShaking * shake * randomNumber;
 
 	//reduce camera trauma in time
 	this->m_trauma = dt * this->trauma_reducing;
--- Top-Down Game/Top-Down Game/Camera.h
+++ Top-Down Game/Top-Down Game/Camera.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef CAMERA_H
 #define CAMERA_H
+#include "Entity.h"
 #include "Utility.h"
 // TODO:
 // AABB Collider
@@ -20,12 +21,42 @@ public:
 	Camera();
 	~Camera();
 	void Update(float dt);
+	void SetViewToTarget(sf::RenderTarget& target);
+
 	void AddCameraShake(float force);
+	void SetCameraShake(float force);
+
+	void SetEntityToFollow(std::weak_ptr<Entity> entity);
+
+	void SetTraumaReducing(float tr);
+	float GetTraumaReducing();
+
+	float GetMaxAngleShaking();
+	void SetMaxAngleShaking(float maxAngle);
+
+	float GetMaxOffSetShaking();
+	void SetMaxOffSetShaking(float offset);
+
+	float GetCameraSmoothness();
+	void SetCameraSmoothness(float smoothness);
+
+	float GetCameraAngle();
+	void SetCameraAngle(float angle);
+
+	sf::Vector2f GetCameraPositionOffSet();
+	void SetCameraPositionOffSet(sf::Vector2f offset);
+
+	float GetCameraTrauma();
 private:
+	sf::Vector2f m_pos;
 	ShakingData ComputeCameraShake(float dt);
+	std::weak_ptr<Entity> followed_entity;
 	sf::View view;
+	float m_angle;
 	float m_trauma;
 	float trauma_reducing;
+	float m_camera_smoothness;
+	sf::Vector2f m_camera_position_offset;
 
 	float maxAngleShaking;
 	float maxOffSetShaking;
